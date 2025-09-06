c02333bb0c3b9d21fef552c3b5940306e869edff
rare-mubin <mvrdark@gmail.com>
2025-09-06 18:49:15 +0600

    change titlebar size

--- ProjectR/Login.Designer.cs
+++ ProjectR/Login.Designer.cs
@@ -54,7 +54,7 @@
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 22);
+            this.panel1.Location = new System.Drawing.Point(0, 28);
             this.panel1.Margin = new System.Windows.Forms.Padding(0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1278, 1);
@@ -68,21 +68,22 @@
             this.pnlTittlebar.Location = new System.Drawing.Point(0, 0);
             this.pnlTittlebar.Margin = new System.Windows.Forms.Padding(0);
             this.pnlTittlebar.Name = "pnlTittlebar";
-            this.pnlTittlebar.Size = new System.Drawing.Size(1278, 22);
+            this.pnlTittlebar.Size = new System.Drawing.Size(1278, 28);
             this.pnlTittlebar.TabIndex = 0;
             // 
             // btnMinimize
             // 
+            this.btnMinimize.Dock = System.Windows.Forms.DockStyle.Right;
             this.btnMinimize.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
             this.btnMinimize.FlatAppearance.BorderSize = 0;
             this.btnMinimize.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnMinimize.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnMinimize.ForeColor = System.Drawing.Color.White;
-            this.btnMinimize.Location = new System.Drawing.Point(1232, 0);
+            this.btnMinimize.Location = new System.Drawing.Point(1222, 0);
             this.btnMinimize.Margin = new System.Windows.Forms.Padding(0);
             this.btnMinimize.Name = "btnMinimize";
-            this.btnMinimize.Size = new System.Drawing.Size(22, 22);
+            this.btnMinimize.Size = new System.Drawing.Size(28, 28);
             this.btnMinimize.TabIndex = 2;
             this.btnMinimize.Text = "-";
             this.btnMinimize.UseVisualStyleBackColor = true;
@@ -96,10 +97,10 @@
             this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnExit.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnExit.ForeColor = System.Drawing.Color.White;
-            this.btnExit.Location = new System.Drawing.Point(1256, 0);
+            this.btnExit.Location = new System.Drawing.Point(1250, 0);
             this.btnExit.Margin = new System.Windows.Forms.Padding(0);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(22, 22);
+            this.btnExit.Size = new System.Drawing.Size(28, 28);
             this.btnExit.TabIndex = 1;
             this.btnExit.Text = "X";
             this.btnExit.UseVisualStyleBackColor = true;
