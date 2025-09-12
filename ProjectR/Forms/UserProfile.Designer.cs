68faebaeea276f3c284cf13cf7bc7be1e9cae247
nazmussakib0203 <sakibn916@gmail.com>
2025-09-13 00:14:01 +0600

    add user profile page

--- ProjectR/Forms/UserProfile.Designer.cs
+++ ProjectR/Forms/UserProfile.Designer.cs
@@ -28,20 +28,189 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtTotalSells = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtNidNumber = new System.Windows.Forms.TextBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.ldlNidNumber = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.lblTotalSells = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(79, 38);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(190, 190);
+            this.panel2.TabIndex = 3;
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(172)))), ((int)(((byte)(169)))), ((int)(((byte)(183)))));
+            this.lblUserName.Location = new System.Drawing.Point(196, 223);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(179, 46);
+            this.lblUserName.TabIndex = 1;
+            this.lblUserName.Text = "User Name";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(172)))), ((int)(((byte)(169)))), ((int)(((byte)(183)))));
+            this.label2.Location = new System.Drawing.Point(326, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(369, 55);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "User Name";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(58)))), ((int)(((byte)(204)))));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.txtTotalSells);
+            this.panel1.Controls.Add(this.txtPhone);
+            this.panel1.Controls.Add(this.txtNidNumber);
+            this.panel1.Controls.Add(this.txtUserName);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.ldlNidNumber);
+            this.panel1.Controls.Add(this.lblPhone);
+            this.panel1.Controls.Add(this.lblTotalSells);
+            this.panel1.Controls.Add(this.lblUserName);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(3, 137);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1094, 540);
+            this.panel1.TabIndex = 2;
+            // 
+            // txtTotalSells
+            // 
+            this.txtTotalSells.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotalSells.Location = new System.Drawing.Point(674, 427);
+            this.txtTotalSells.Multiline = true;
+            this.txtTotalSells.Name = "txtTotalSells";
+            this.txtTotalSells.Size = new System.Drawing.Size(199, 40);
+            this.txtTotalSells.TabIndex = 14;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPhone.Location = new System.Drawing.Point(674, 359);
+            this.txtPhone.Multiline = true;
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(199, 40);
+            this.txtPhone.TabIndex = 13;
+            // 
+            // txtNidNumber
+            // 
+            this.txtNidNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNidNumber.Location = new System.Drawing.Point(674, 298);
+            this.txtNidNumber.Multiline = true;
+            this.txtNidNumber.Name = "txtNidNumber";
+            this.txtNidNumber.Size = new System.Drawing.Size(199, 40);
+            this.txtNidNumber.TabIndex = 12;
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.BackColor = System.Drawing.SystemColors.Window;
+            this.txtUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserName.Location = new System.Drawing.Point(674, 229);
+            this.txtUserName.Multiline = true;
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(199, 40);
+            this.txtUserName.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(78)))), ((int)(((byte)(78)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(933, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 29);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Edit Profile";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label10
+            // 
+            this.label10.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(172)))), ((int)(((byte)(169)))), ((int)(((byte)(183)))));
+            this.label10.Location = new System.Drawing.Point(328, 103);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(130, 46);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Admin";
+            // 
+            // ldlNidNumber
+            // 
+            this.ldlNidNumber.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ldlNidNumber.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(172)))), ((int)(((byte)(169)))), ((int)(((byte)(183)))));
+            this.ldlNidNumber.Location = new System.Drawing.Point(196, 292);
+            this.ldlNidNumber.Name = "ldlNidNumber";
+            this.ldlNidNumber.Size = new System.Drawing.Size(179, 46);
+            this.ldlNidNumber.TabIndex = 4;
+            this.ldlNidNumber.Text = "Nid Number";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPhone.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(172)))), ((int)(((byte)(169)))), ((int)(((byte)(183)))));
+            this.lblPhone.Location = new System.Drawing.Point(196, 362);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(179, 46);
+            this.lblPhone.TabIndex = 3;
+            this.lblPhone.Text = "Phone Number";
+            // 
+            // lblTotalSells
+            // 
+            this.lblTotalSells.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalSells.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(172)))), ((int)(((byte)(169)))), ((int)(((byte)(183)))));
+            this.lblTotalSells.Location = new System.Drawing.Point(196, 430);
+            this.lblTotalSells.Name = "lblTotalSells";
+            this.lblTotalSells.Size = new System.Drawing.Size(179, 46);
+            this.lblTotalSells.TabIndex = 2;
+            this.lblTotalSells.Text = "Total Sells";
             // 
             // UserProfile
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::ProjectR.Properties.Resources.usercontrolBackground;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Margin = new System.Windows.Forms.Padding(0);
             this.Name = "UserProfile";
             this.Size = new System.Drawing.Size(1100, 690);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtTotalSells;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtNidNumber;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label ldlNidNumber;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Label lblTotalSells;
     }
 }
